b8c953e1256607e830a8047cdc5302f8d5252c57
Chaitanya <67656388+ChhikaraBRUH@users.noreply.github.com>
2022-03-24 17:28:30 +0530

    feat: added private and public routing rules

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/auth/authContext";
+
+const PrivateRoute = ({ children }) => {
+	const { isAuth } = useAuth();
+
+	return isAuth ? children : <Navigate to='/login' />;
+};
+
+export { PrivateRoute };
